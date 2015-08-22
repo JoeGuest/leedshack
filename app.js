40af3390d12b78a197a755848d7afb9f9416540d
petecoop <pete@petecoop.co.uk>
2015-08-22 16:13:12 +0100

    get games and stats

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-
-
 var express = require('express'),
-  config = require('./config/config')
+  config = require('./config/config'),
+  glob = require('glob');
 
 var app = express();
 
 require('./config/express')(app, config);
+
+//spawn job processes
+var jobs = glob.sync(config.root + '/app/jobs/*.js');
+jobs.forEach(function (job) {
+  require(job);
+});
 
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
